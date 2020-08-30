64d9dc674cd9c72b0955a26ad9434712d5115b81
Johnny Thai <jthai04@gmail.com>
2020-08-29 23:17:03 -0700

    commit test

--- waitlist.py
+++ waitlist.py
@@ -11,86 +11,18 @@ a waiting guest will be removed from waiting table and  moved to the seated tabl
 connector() is a method that instantiates an object using the definition found in class
 """
 
-# username = input('Enter username: ')
-# password = getpass('Logging into \'{user}\'...\nEnter password: ').format(user=username)
+username = input('Enter username: ')
+password = getpass('Logging into \'{user}\'...\nEnter password: ').format(user=username)
 
-# conn = mysql.connector.connect(
-#     host="127.0.0.1",
-#     user=username,
-#     password=password,
-#     database="waitlist"
-# )
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user=username,
+    password=password,
+    database="waitlist"
+)
 
-# mycursor = conn.cursor()
+mycursor = conn.cursor()
 
-class Party:
-    """
-    Created Party object whenever guests check in.
-    """
-
-    def __init__(self, date, party_num, name, size, phone, wait_time):
-        self.date = date
-        self.party_num = party_num
-        self.name = name
-        self.size = size
-        self.phone = phone
-        self.wait_time = wait_time
-
-        def __str__(self):
-            return self.name
-
-    def show_party(self):
-        """
-        Function to print table info.
-        """
-
-        print(
-            '\nParty Number ', self.party_num,
-            '\n     Name: ', self.name,
-            '\n     Size: ', self.size,
-            '\n     Phone: ', self.phone,
-            '\n     Wait Time: ', self.wait_time, ' minutes'
-        )
-
-    def time_seated(self):
-        """
-        Time (minutes) table will be occupied after being seated. A function of party size.
-        Returns eat_time.
-        """
-
-        if datetime.datetime.now().hour <= 16:
-            '''
-            Lunch time is before 4pm.
-            '''
-
-            if self.size <= 4:
-                eat_time = 60
-                return eat_time
-
-            elif self.size < 8:
-                eat_time = 75
-                return eat_time
-            
-            else:
-                eat_time = 90
-                return eat_time
-
-        if datetime.datetime.now().hour > 16:
-            '''
-            Dinner time is after 4pm.
-            '''
-
-            if self.size <= 4:
-                eat_time = 90
-                return eat_time
-
-            elif self.size < 8:
-                eat_time = 105
-                return eat_time
-            
-            else:
-                eat_time = 120
-                return eat_time
 
 def commit():
     """
